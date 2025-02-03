76688ff3384a0fc3b66b31511f253bcef240a7a1
Hayron Rodrigues Neves <115109475+Hayron1206@users.noreply.github.com>
2025-02-03 09:32:28 -0300

    Delete public directory

--- public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "../app/application.php";
-use Fmk\Utils\Request;
-echo Request::exec();
-
-
-// echo Router::getRouteByName('funcionario.gorjeta')
-// ->setParamns(['funcionario_id'=>15,'data'=>date("Y-m-d")]);
-
